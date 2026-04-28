87b53333c42ecd0a4866d3fdcceaafbfe851fc6f
Rachel <tungxeko912@gmail.com>
2026-04-28 21:55:55 +0000

    days 1 new learning zig

---
+++ days1/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("Hello, world!\n", .{});
+    std.debug.print("Hello well come to Zig, my name is 0xcRachel Aizo Chilll\n", .{});
+}
