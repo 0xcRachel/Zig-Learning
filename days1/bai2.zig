92d494a89376ae3f62561b685bf0658e0b07d4be
Rachel <tungxeko912@gmail.com>
2026-04-29 19:39:35 +0000

    while and for

---
+++ days1/bai2.zig
@@ -0,0 +1,21 @@
+// while and for looping array
+
+const std = @import("std");
+
+fn test1() !void {
+    var i: u8 = 0;
+    while (i < 10) : (i += 1) {
+        std.debug.print("{}\n", .{i});
+    }
+}
+
+fn test2() !void {
+    var i: u8 = 0;
+    while (i < 100) : (i += 1) {
+        std.debug.print(("0xcRachel\n"), .{});
+    }
+}
+
+pub fn main() !void {
+    try test2();
+}
