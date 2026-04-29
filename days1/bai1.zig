d39a3a48df3f11fe092ffeb8cad1c6844eb0c97b
Rachel <tungxeko912@gmail.com>
2026-04-29 15:57:03 +0000

    ifelse

--- days1/bai1.zig
+++ days1/bai1.zig
@@ -8,5 +8,11 @@ pub fn main() !void {
     const desc = "Top 200 HackerRank, Discord Developer, OpenSoucre";
     std.debug.print("Hello Guys, 0xcRachel here well come to zig learning\n", .{});
     std.debug.print("My name is 0xcRachel , age : {} , skill : Zig, Rust, Laravel, Reactjs , NextJS , NESTJS, NODEJS, JAVA, PYTHON and Lua\n", .{age});
-    std.debug.print("my name : {s} , age : {} , skill : {s} , desc : {s}", .{ name, age, skill, desc });
+    std.debug.print("my name : {s} , age : {} , skill : {s} , desc : {s} \n", .{ name, age, skill, desc });
+
+    if (age >= 18) {
+        std.debug.print("You are Anthony {s}\n", .{name});
+    } else {
+        std.debug.print("You are Goth Girl ?", .{});
+    }
 }
